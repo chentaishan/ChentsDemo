5b001fd582f17ca804ce70c056dcd2f38199207e
chentaishan <chentaishan-19@163.com>
2019-04-21 22:54:09 +0800

    Initial commit

---
+++ app/src/main/java/chents/example/cts/chentsdemo/MainActivity.java
@@ -0,0 +1,38 @@
+package chents.example.cts.chentsdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.umeng.analytics.MobclickAgent;
+import com.umeng.socialize.UMShareAPI;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ app/src/main/java/chents/example/cts/chentsdemo/wxapi/WXEntryActivity.java
@@ -0,0 +1,6 @@
+package chents.example.cts.chentsdemo.wxapi;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+public class WXEntryActivity extends WXCallbackActivity {
+}
