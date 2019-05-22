21d7c1ec90fb6f5f74b60cdc30836b59ef1786a8
chents <chentaishan-19@163.com>
2019-05-22 10:10:52 +0800

    git test init
    (cherry picked from commit bc54361a1fa05e927bae0cbd882e5e676f5e9366)

--- gittest/src/main/java/com/example/gittest/MainActivity.java
+++ gittest/src/main/java/com/example/gittest/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.gittest;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
 
         Intent intent = new Intent();
