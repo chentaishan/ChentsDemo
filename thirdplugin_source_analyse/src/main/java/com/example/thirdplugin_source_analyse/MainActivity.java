ed914defd77b885a86c656530da447f569214d40
chents <chentaishan-19@163.com>
2019-11-07 20:43:46 +0800

    添加 picasso 研究

---
+++ thirdplugin_source_analyse/src/main/java/com/example/thirdplugin_source_analyse/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.thirdplugin_source_analyse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Picasso.with(this).load("").into(new ImageView(this));
+    }
+}
